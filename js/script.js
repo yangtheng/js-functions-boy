0ed4f6d468fea97988b6f6b281f1af0521d1e37d
Prima Aulia Gusta <primaulia@gmail.com>
2017-06-23 08:57:29 +0800

    latest one

--- js/script.js
+++ js/script.js
@@ -1,14 +1,52 @@
-console.log('trying some changes')
+// update the h1 to say higher or lower
+// or if the number guess is correct, we change the body color
 
-// function pausecomp (millis) {
-//   var date = new Date()
-//   var curDate = null
-//
-//   while (curDate - date < millis) {
-//     curDate = new Date()
-//   }
-//
-//   alert('done pausing')
-// }
-//
-// pausecomp(3000)
+var randomNumber = randomFn(0, 10)
+var guessedNum = prompt('The first guess: type your number!')
+var isCorrect = false
+var gameOver = false
+
+function randomFn (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
+}
+
+function updateH1(newH1) {
+  var h1 = document.querySelector('h1')
+  // updated the property of h1 dom node
+  h1.textContent = newH1
+}
+
+// fn that checks numbers with the random number
+function askForANumber (guessedNum, randomNumber) {
+  guessedNum = parseInt(guessedNum)
+
+  if (guessedNum > randomNumber) {
+    updateH1('lower, randomNumber is ' + randomNumber)
+  } else if (guessedNum < randomNumber) {
+    updateH1('higher, randomNumber is ' + randomNumber)
+  }
+
+  if( guessedNum === randomNumber) {
+    updateH1('correct')
+    gameOver = true
+  }
+
+  checkForGameover()
+}
+
+function newGame() {
+  askForANumber( guessedNum, 5 )
+}
+
+function checkForGameover() {
+  if(gameOver) {
+    alert('reveal the restart button now, stop the game')
+  } else {
+    guessedNum = prompt('Type your number again!')
+    askForANumber(guessedNum, 5)
+  }
+}
+
+if(! gameOver) {
+  newGame()
+}
